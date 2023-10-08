0d629ae8ee27c8c631356e335325838994f2a43c
adreannalaporte <92052358+adreannalaporte@users.noreply.github.com>
2023-10-07 22:41:11 -0700

    Update Linear_Regression.R

--- Linear_Regression.R
+++ Linear_Regression.R
@@ -1,9 +1,8 @@
-# ISE 225 - Engineering Statistics Project 
+# ISE 225 - Engineering Statistics
+# LINEAR REGRESSION
 
 
-# LINEAR REGRESSION: 
-
-# Part 1: 
+# Part 1: Load the wage data & generate a histogram
 
 library(ISLR)
 
@@ -13,8 +12,10 @@ data("Wage")
 
 hist(Wage$wage)
 
+# The data is left-skewed, suggesting that it is more common to have a below average wage. 
 
-# Part 2: 
+
+# Part 2: Log-transform the wage data, fit a simple linear regression, & summarize the data
 
 log.wage <- log(Wage$wage)
 
@@ -23,19 +24,22 @@ lm.fit_log.wage_age <- lm(log.wage~age, data=Wage)
 summary(lm.fit_log.wage_age)
 
 
-# Part 3:
+# Part 3: Generate the residuals plot
 
 plot(lm.fit_log.wage_age)
 
+# The residuals plot suggests that there is a relationship between the residuals and their predicted outputs
+# meaning we cannot assume that the relationship is linear.
 
-# Part 4: 
+
+# Part 4: Add age to the simple linear regression model
 
 lm.fit_log.wage_2ndorder <- lm(log.wage~age+I(age^2), data=Wage)
 
 summary(lm.fit_log.wage_2ndorder)
 
 
-# Part 5:  
+# Part 5:  Conducting a forward selection and decide which are the best predictors of wage. 
 
 library(leaps)
 install.packages("glmnet")
@@ -48,7 +52,7 @@ forward.selection <- regsubsets(log.wage~ age+ year+ maritl+ race+ education+ jo
 summary(forward.selection)
 
 
-# Part 6: 
+# Part 6: Preparing the data for a Lasso Feature Selection
 
 set.seed(123)
 
@@ -62,7 +66,7 @@ y.test <- y[-train]
 
 
 
-# Part 7:
+# Part 7: Conducting a Lasso Analysis to select the best features 
 
 grid <- 10^seq(10, -2, length=100)
 lasso <- glmnet(x[train,], y[train], alpha=1, lambda = grid)
